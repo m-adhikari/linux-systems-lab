4c518fe123467c83a4f50db0d1e9116c43904557
Manish Adhikari <adhikari.og@gmail.com>
2026-08-08 23:31:18 -0500

    replaced printf() with write()

--- mini-ls/src/main.c
+++ mini-ls/src/main.c
@@ -1,31 +1,49 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+
+size_t string_length(const char *str)
+{
+	size_t length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	
+	return length;
+}
 
 int main(int argc, char *argv[])
-{	
+{
 	char *path = ".";
 
-	printf("Parsed values: \n");		
+	write(1, "Parse values \n", 15);
 
 	if (argc > 1)
 	{
-		for (int i = 1; i < argc; ++i)
+		for (int i = 0; i < argc; ++i)
 		{
 			if (argv[i][0] == '-')
 			{
-				printf("Flag: %s \n", argv[i]);
-			}	
-			else 
+				write(1, "Flag: ", 6);
+				write(1, argv[i], string_length(argv[i]));
+				write(1, "\n", 1);
+			}
+			else
 			{
-				printf("Path: %s \n", argv[i]);  
+				write(1, "Path: ", 6);
+				write(1, argv[i], string_length(argv[i]));
+				write(1, "\n", 1);
+
 				path = argv[i];
 			}
 		}
 
 	}
 
-	printf("\n");
-	printf("Final path: %s \n", path);
+	write (1, "\n Final path: ", 13);
+	write (1, path, string_length(path));
+	write (1, "\n", 1);
 
 	return 0;
 }
-
